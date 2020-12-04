6925771ee167d0a9465257a31f8f874f81caec7e
@s.roertgen <sroertgen@gmail.com>
2020-12-04 11:33:21 +0100

    add script to auto convert collection based on ids.json file

---
+++ auto-convert-collection-to-skos.py
@@ -0,0 +1,178 @@
+import requests
+import json
+from rdflib import Namespace, Graph, URIRef, SKOS, RDF, DCTERMS, Literal
+from rdflib.namespace import SDO
+from pprint import pprint
+import json
+
+base_url = "https://redaktion.openeduhub.net/edu-sharing/rest/collection/v1/collections/-home-/"
+appendix = "/children/collections/"
+main_collection_id = "5e40e372-735c-4b17-bbf7-e827a5702b57"
+main_collection = {
+    "id": main_collection_id,
+    "prefLabel": "Main Collection",
+    "children": []
+}
+
+topic_identifier = [
+    "Aktiviert / Sichtbar",
+    "activated",
+    ""
+]
+
+
+def getRequest(url):
+    r = requests.get(url,
+                     params={
+                         "scope": "MY",
+                         "maxItems": "999999",
+                         "skipCount": 0,
+                         "propertyFilter": "-all-"
+                     },
+                     headers={
+                         "Accept": "application/json"
+                     }
+                     )
+    response = r.json()
+    return response
+
+
+def getCollection(url):
+    response = getRequest(url)
+
+    collections = []
+    for item in response["collections"]:
+        if any(x in topic_identifier for x in item["properties"]["ccm:editorial_state_DISPLAYNAME"]):
+            collection = {
+                "id": item["ref"]["id"],
+                "prefLabel": item["title"],
+                "keywords": item.get("properties").get("cclom:general_keyword", []),
+                "discipline": item.get("properties").get("ccm:taxonid", ""),
+                "educationalContext": item.get("properties").get("ccm:educationalcontext", ""),
+                "children": []
+            }
+        else:
+            continue
+        collections.append(collection)
+    return collections
+
+
+def get_all_collections(url, parent_collection):
+    for collection in getCollection(url):
+        print(f"{collection['id']} : {collection['prefLabel']}")
+        parent_collection["children"].append(collection)
+        get_all_collections(base_url + collection["id"] + appendix, collection)
+
+
+def getMainCollections(url, collection):
+    main_ids = {}
+    for collection in getCollection(url):
+        main_ids.update({collection['id']: collection['prefLabel']})
+    return main_ids
+
+
+def buildGraph(data, start_id, collection_name):
+    g = Graph()
+    name = collection_name.lower().replace(" ", "-")
+    base_url = URIRef("http://w3id.org/openeduhub/vocabs/oeh-topics/")
+    concept_scheme_url = URIRef(
+        "http://w3id.org/openeduhub/vocabs/oeh-topics/" + start_id)
+    OEHTOPICS = Namespace(base_url)
+
+    g.add((concept_scheme_url, RDF.type, SKOS.ConceptScheme))
+    g.add((concept_scheme_url, DCTERMS.creator, Literal(
+        "WirLernenOnline Fachportalmanger:innen", lang="de")))
+    g.add((concept_scheme_url, DCTERMS.title, Literal(collection_name, lang="de")))
+
+    # get main items and add them as top concepts
+    topConcepts = [concept["id"] for concept in data["children"]]
+
+    def parseTree(tree):
+        # if id is ConceptScheme, skip
+        item_url = base_url + URIRef(tree["id"])
+        if tree["id"] == start_id and len(tree["children"]):
+            for item in tree["children"]:
+                # child_url = URIRef(base_url + item["id"])
+                # g.add((base_url, SKOS.narrower, child_url))
+                # g.add((child_url, SKOS.broader, base_url))
+                parseTree(item)
+        else:
+            g.add((item_url, RDF.type, SKOS.Concept))
+            g.add((item_url, SKOS.prefLabel, Literal(
+                tree["prefLabel"], lang="de")))
+
+            if "hiddenLabel" in tree.keys():
+                for item in tree["hiddenLabel"]:
+                    g.add((item_url, SKOS.hiddenLabel, Literal(item, lang="de")))
+
+            if "keywords" in tree.keys():
+                for item in tree["keywords"]:
+                    g.add((item_url, SDO.keywords, Literal(item, lang="de")))
+
+            if "discipline" in tree.keys():
+                for item in tree["discipline"]:
+                    # g.add( (item_url, SDO.about, item))
+                    g.add((item_url, SKOS.related, URIRef(item)))
+
+            if tree["id"] in topConcepts:
+                g.add((item_url, SKOS.topConceptOf, concept_scheme_url))
+                g.add((concept_scheme_url, SKOS.hasTopConcept, item_url))
+            else:
+                g.add((item_url, SKOS.inScheme, concept_scheme_url))
+
+            if len(tree["children"]):
+                for item in tree["children"]:
+                    child_url = URIRef(base_url + item["id"])
+                    g.add((item_url, SKOS.narrower, child_url))
+                    g.add((child_url, SKOS.broader, item_url))
+                    parseTree(item)
+
+    parseTree(data)
+
+    g.bind("skos", SKOS),
+    g.bind("oeh", OEHTOPICS)
+    g.bind("dct", DCTERMS)
+    g.bind("sdo", SDO)
+
+    output = g.serialize(format="turtle", base=base_url).decode("utf-8")
+
+    with open("data/graph_" + name + ".ttl", "w") as f:
+        f.write(output)
+
+
+def read_ids() -> list:
+    with open("ids.json") as f:
+        ids = list(json.load(f).values())
+        return ids
+
+
+def parse_collection_id(_id):
+    start_id = _id
+    collection_name = main_ids[start_id]
+    print(f"converting: {collection_name}")
+    url = base_url + start_id + appendix
+
+    result_collection: dict = {
+        "id": start_id,
+        "prefLabel": collection_name,
+        "children": []
+    }
+    get_all_collections(url, result_collection)
+    buildGraph(data=result_collection, start_id=start_id,
+               collection_name=collection_name)
+
+###
+
+
+# read ids
+ids = read_ids()
+
+# get all collections
+start_url = base_url + main_collection_id + appendix
+main_ids = getMainCollections(start_url, main_collection)
+
+# add Main collection to main ids
+main_ids.update({main_collection_id: "Main-Collection (All)"})
+
+for _id in ids:
+    parse_collection_id(_id)

ce76e9e27d1d47199aa52e9e1006d19629e0caf4
@s.roertgen <sroertgen@gmail.com>
2022-07-01 14:35:22 +0200

    change print statement to logger

--- src/base_logger.py
+++ src/base_logger.py
@@ -1,7 +1,16 @@
 import logging
-from settings import LOG_LEVEL
+import os
+
+
+def get_log_level():
+    log_level = os.environ.get("log_level", logging.INFO)
+
+    if log_level == "debug":
+        return logging.DEBUG
+    return logging.INFO
 
 logger = logging
+LOG_LEVEL = get_log_level()
 
 # log to file and stdout
 logger.basicConfig(
--- src/settings.py
+++ src/settings.py
@@ -3,22 +3,15 @@ import logging
 
 from dotenv import load_dotenv
 
+from base_logger import logger
 
 load_dotenv()
-
-def get_log_level():
-    log_level = os.environ.get("log_level", logging.INFO)
-
-    if log_level == "debug":
-        return logging.DEBUG
-    return logging.INFO
 
 START_COLLECTION_ID = os.environ.get("start_collection_id", None)
 START_COLLECTION_NAME = os.environ.get("default_name_for_start_collection", "")
 GITHUB_TOKEN = os.environ.get("github_token", "")
 GIT_REPO = os.environ.get("git_repo", "")
 FILENAME_FOR_PUSH = os.environ.get("filename_for_push", "oehTopics.ttl")
-LOG_LEVEL = get_log_level()
 DRY_RUN = eval(os.environ.get("dry_run", "True"))
 
 errors = []
@@ -35,13 +28,12 @@ if len(errors) != 0:
     raise Exception("Necessary environment variables are missing")
 
 def show_configs():
-    print(f"START_COLLECTION_ID: {START_COLLECTION_ID}")
-    print(f"START_COLLECTION_NAME: {START_COLLECTION_NAME}")
-    print(f"GITHUB_TOKEN: {GITHUB_TOKEN}")
-    print(f"GIT_REPO: {GIT_REPO}")
-    print(f"FILENAME_FOR_PUSH: {FILENAME_FOR_PUSH}")
-    print(f"LOG_LEVEL: {LOG_LEVEL}")
-    print(f"DRY_RUN: {DRY_RUN}")
+    logger.info(f"START_COLLECTION_ID: {START_COLLECTION_ID}")
+    logger.info(f"START_COLLECTION_NAME: {START_COLLECTION_NAME}")
+    logger.info(f"GITHUB_TOKEN: {GITHUB_TOKEN}")
+    logger.info(f"GIT_REPO: {GIT_REPO}")
+    logger.info(f"FILENAME_FOR_PUSH: {FILENAME_FOR_PUSH}")
+    logger.info(f"DRY_RUN: {DRY_RUN}")
 
 
 show_configs()

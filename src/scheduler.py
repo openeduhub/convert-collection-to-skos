be8aea5e89a27338f04323210f14a7aff53161cf
Torsten Simon <simon@edu-sharing.net>
2023-05-03 16:10:50 +0200

    no job, repeat via external container runs

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,8 +1,3 @@
-import time
-from datetime import datetime, timedelta
-
-import schedule
-
 from auto_convert_collection_to_skos import parse_collection_id
 from base_logger import logger
 from git_pusher import push_to_github
@@ -10,19 +5,12 @@ from housekeeping import housekeeping
 
 
 def job():
-    start_time = datetime.now()
     parse_collection_id()
     push_to_github()
     housekeeping()
-    logger.info(f"Next job will start at: {start_time + timedelta(hours=1)}")
+    logger.info("Finished")
 
 
 def run_scheduler():
-    # execute job every second hour
-    job_start_time = datetime.now() + timedelta(minutes=1)
-    logger.info(f"Job will start at: {job_start_time.strftime('%I:%M%p')}")
-    schedule.every().hour.at(":" + job_start_time.strftime('%M')).do(job)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # job cycle is done via external infrastructure
+    job()
